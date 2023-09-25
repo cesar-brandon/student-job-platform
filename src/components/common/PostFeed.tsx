26c4c69aef11821c14105f76e2c454aec8826628
Cesar Brandon <cesarbrandon000@gmail.com>
2023-09-25 09:32:45 -0500

    :bug: Author's image not null

--- src/components/common/PostFeed.tsx
+++ src/components/common/PostFeed.tsx
@@ -69,7 +69,7 @@ const PostFeed = ({ initialPosts, authorName }: PostFeedProps) => {
                 post={post}
                 commentAmt={post.comments.length}
                 authorName={post.author.name}
-                authorImage={post.author.image}
+                authorImage={post.author.image || ""}
                 votesAmt={votesAmt}
                 currentVote={currentVote}
               />
@@ -82,7 +82,7 @@ const PostFeed = ({ initialPosts, authorName }: PostFeedProps) => {
               post={post}
               commentAmt={post.comments.length}
               authorName={post.author.name}
-              authorImage={post.author.image}
+              authorImage={post.author.image || ""}
               votesAmt={votesAmt}
               currentVote={currentVote}
             />
@@ -94,18 +94,3 @@ const PostFeed = ({ initialPosts, authorName }: PostFeedProps) => {
 };
 
 export default PostFeed;
-
-// <div className="flex flex-col gap-4 px-4 sm:p-0">
-//   <div className="w-full min-h-[8rem] bg-white rounded-3xl p-4 hover:drop-shadow-xl transition">
-//     <strong className="text-blue-500 text-lg">Titulo</strong>
-//     <div className="flex gap-2">
-//       <p className="font-bold">empresa -</p>
-//
-//       <p>fecha de publicacion</p>
-//     </div>
-//     <p className="text-sm">
-//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, fuga
-//       deleniti! Excepturi nostrum molestiae unde natus optio.
-//     </p>
-//   </div>
-// </div>
