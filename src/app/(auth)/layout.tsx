709b38cec4b2c0593eb9f370b7a92e0133537b8c
Cesar Brandon <cesarbrandon000@gmail.com>
2024-03-24 22:54:51 -0500

    :bug: React Email in Nextjs 14 (close #35)

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -24,7 +24,7 @@ export default async function AuthLayout({
     <section className="flex flex-col md:flex-row h-screen items-center">
       <BackButton type="chevron" />
       <div
-        className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        className="bg-background w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
         flex items-center justify-center"
       >
         {children}
