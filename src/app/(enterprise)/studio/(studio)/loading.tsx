99081578e6c78d9f19e48ea047756636e98f49f3
Cesar Brandon <cesarbrandon000@gmail.com>
2024-06-23 21:50:36 -0500

    :recycle: Directory change and middleware for enterprise routing

---
+++ src/app/(enterprise)/studio/(studio)/loading.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return <Skeleton className="w-42 h-42">Loadingggggg</Skeleton>;
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/home"],
+  matcher: ["/home", "/studio"],
 };
